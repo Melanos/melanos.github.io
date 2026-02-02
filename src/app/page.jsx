7af5d0ec0772efdc42bca1404ba0a9705ecf670e
Melanos <aikanaro@live.ru>
2026-02-02 09:21:06 -0500

    Enable photo gallery on main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,11 +15,11 @@ import logoAirbnb from '@/images/logos/progressive-3.svg'
 // import logoFacebook from '@/images/logos/facebook.svg'
 import logoPlanetaria from '@/images/logos/progressive-3.svg'
 // import logoStarbucks from '@/images/logos/starbucks.svg'
-// import image1 from '@/images/photos/image-1.jpg'
-// import image2 from '@/images/photos/image-2.jpg'
-// import image3 from '@/images/photos/image-3.jpg'
-// import image4 from '@/images/photos/image-4.jpg'
-// import image5 from '@/images/photos/image-5.jpg'
+import image1 from '@/images/photos/image-1.jpg'
+import image2 from '@/images/photos/image-2.jpg'
+import image3 from '@/images/photos/image-3.jpg'
+import image4 from '@/images/photos/image-4.jpg'
+import image5 from '@/images/photos/image-5.jpg'
 import logoProgressive from '@/images/logos/progressive-3.svg'
 import logoETS from '@/images/logos/progressive-3.svg'  // Using same logo for now
 import logoPython from '@/images/logos/python.svg'
@@ -247,32 +247,32 @@ function Resume() {
   )
 }
 
-// function Photos() {
-//   let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+function Photos() {
+  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
 
-//   return (
-//     <div className="mt-16 sm:mt-20">
-//       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-//         {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-//           <div
-//             key={image.src}
-//             className={clsx(
-//               'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-//               rotations[imageIndex % rotations.length],
-//             )}
-//           >
-//             <Image
-//               src={image}
-//               alt=""
-//               sizes="(min-width: 640px) 18rem, 11rem"
-//               className="absolute inset-0 h-full w-full object-cover"
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
+  return (
+    <div className="mt-16 sm:mt-20">
+      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+          <div
+            key={image.src}
+            className={clsx(
+              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
+              rotations[imageIndex % rotations.length],
+            )}
+          >
+            <Image
+              src={image}
+              alt=""
+              sizes="(min-width: 640px) 18rem, 11rem"
+              className="absolute inset-0 h-full w-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default async function Home() {
   let articles = (await getAllArticles()).slice(0, 4)
@@ -288,7 +288,7 @@ Enterprise Automation Engineer & AI Operations Specialist.          </h1>
           </p>
         </div>
       </Container>
-      {/* <Photos /> */}
+      <Photos />
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
